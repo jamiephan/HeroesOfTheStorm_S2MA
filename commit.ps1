fc26768d3e0760c598b0536d063126e6ec1cd5e5
Jamie Phan <9488815+jamiephan@users.noreply.github.com>
2021-01-04 14:22:37 +0000

    Updated S2MAs _ 25614

--- commit.ps1
+++
@@ -1,4 +0,0 @@
-$time = [int][double]::Parse((Get-Date -UFormat %s))
-Start-Process "git" -ArgumentList "add","-A" -NoNewWindow -PassThru| Wait-Process
-Start-Process "git" -ArgumentList "commit","-m", "[$time] ⬆ Updated S2MA + Maps" -NoNewWindow -PassThru| Wait-Process
-Start-Process "git" -ArgumentList "push" -NoNewWindow -PassThru| Wait-Process
